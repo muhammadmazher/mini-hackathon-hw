4c75ab834655bfd40bdcc0213bc678fcdb9efa29
Muhammad Mazher Khan <161349044+muhammadmazher@users.noreply.github.com>
2024-11-02 10:09:37 +0500

    Add files via upload

--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -4,14 +4,14 @@ export default function Herosection () {
   return (
     <div className="">
 
-      <div className="flex justify-center text-3xl border-r text-slate-700 font-semibold py-10">
+      <div className="flex justify-center text-3xl text-slate-700 font-semibold py-10">
         <h1>Sell Your Car on PakWheels and Get the Best Price</h1>
       </div>
 
-      <div className="flex justify-center space-x-12 pb-10">
+      <div className="flex justify-center space-x-12 pb-8">
           <div className="">
           
-            <h1 className="text-xl text-blue-700 font-semibold py-2">Post your Ad on PakWheels</h1>
+            <h1 className="text-lg text-blue-700 font-semibold py-2">Post your Ad on PakWheels</h1>
             <p className="py-1 text-slate-700 font-semibold flex gap-1"><Image src="/check.png" alt="ok" width={25} height={10}/>Post your Ad for Free in 3 Easy Steps</p>
             <p className="py-1 text-slate-700 font-semibold flex gap-1"><Image src="/check.png" alt="ok" width={25} height={10}/>Get Genuine offers form Verified Buyers</p>
             <p className="py-1 text-slate-700 font-semibold flex gap-1"><Image src="/check.png" alt="ok" width={25} height={10}/>Sell your car Fast at the Best Price</p>
@@ -26,7 +26,7 @@ export default function Herosection () {
               <h1 className="text-slate-400 text-xl font-semibold py-20">OR</h1>
           </div>
 
-          <div>
+          <div className="pl-2">
           <h1 className="text-lg text-blue-700 font-semibold py-2">Try PakWheels Sell it For Me</h1>
             <p className="py-1 text-slate-700 font-semibold flex gap-1"><Image src="/check.png" alt="ok" width={25} height={10}/>Dedicated Sales Expert to Sell your Car</p>
             <p className="py-1 text-slate-700 font-semibold flex gap-1"><Image src="/check.png" alt="ok" width={25} height={10}/>We Bargain for you and share the Best Offer</p>
